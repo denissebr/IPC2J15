19c97e6a991b1645217a18397c83e13e62acf47c
denissebr <joziitha642@gmail.com>
2015-06-13 00:37:21 -0600

    pagina de inicio

--- FASE 2/[IPC2]FASE2_201224240/[IPC2]FASE2_201224240/Default.aspx
+++ FASE 2/[IPC2]FASE2_201224240/[IPC2]FASE2_201224240/Default.aspx
@@ -1,23 +1,109 @@
 ﻿<%@ Page Title="QUTEZAL EXPRESS" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_IPC2_FASE2_201224240._Default" %>
 
 <asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
-  <ul class="nav nav-pills">
-  <li class="active"><a href="#">Home</a></li>
-  <li><a href="#">Profile</a></li>
-  <li class="disabled"><a href="#">Disabled</a></li>
-  <li class="dropdown">
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-      Dropdown <span class="caret"></span>
-    </a>
-    <ul class="dropdown-menu">
-      <li><a href="#">Action</a></li>
-      <li><a href="#">Another action</a></li>
-      <li><a href="#">Something else here</a></li>
-      <li class="divider"></li>
-      <li><a href="#">Separated link</a></li>
-    </ul>
-  </li>
-</ul> 
+  
+<head>
+
+<!-- title and meta -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+<meta name="description" content="A neat and simple tabbed content area with CSS and jQuery" />
+<title>Simple Tabbed Content Area with CSS and jQuery</title>
+    
+<!-- css -->
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,700,400italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="Content/base.css" />
+<link rel="stylesheet" href="Content/style.css" />
+    
+<!-- js -->
+ <script src="Scripts/tabs.js"></script>
+<script src="Scripts/jquery-1.9.1.min.js"></script>
+<script src="Scripts/modernizr.js"></script>
 
 
+
+</head>
+
+
+
+<body>
+
+<div id="wrapper">
+
+<div id="top-bar">
+</div><!-- /#top-bar -->
+
+<header>
+    <div id="title" class="container">
+        <h1>QUETZAL EXPRESS</h1>
+    </div>
+</header><!-- /header -->
+
+
+<div id="main">
+    <div class="container">
+        <ul id="tabs">
+            <li class="active">INICIO</li>
+            <li >COTIZAR</li>
+            <li >INICIAR SESION</li>
+            <li>REGISTRAR</li>
+        </ul>
+        <ul id="tab">
+            <li class="active">
+                <h2>RESUMEN DE QUETZAL EXPRESS</h2>
+                <h3>HOLA</h3>
+                <h4>HOLI</h4>
+                <h5>Zzz</h5>
+                 </li>
+            <li >
+            <asp:Label ID="Label2" runat="server" Text="PRECIO"></asp:Label>&nbsp;&nbsp;<asp:TextBox ID="TextBox2" runat="server" Width="201px"></asp:TextBox>&nbsp; <asp:Label ID="Label3" runat="server" Text="US $"></asp:Label>
+           
+            &nbsp;<br />
+                <br />
+           
+            <asp:Label ID="Label1" runat="server" Text="PESO DEL PAQUETE"></asp:Label>&nbsp;&nbsp;<asp:TextBox ID="TextBox1" runat="server" Width="109px"></asp:TextBox>
+           
+            &nbsp;Libras<br />
+                <br />
+           
+            <asp:Label ID="Label4" runat="server" Text="TIPO"></asp:Label>&nbsp;&nbsp;<asp:ListBox ID="ListBox1" runat="server" Height="28px" Width="216px">
+            <asp:ListItem></asp:ListItem>
+                </asp:ListBox>
+           
+                <br />
+                <br />
+                TOTAL A PAGAR:
+                <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+           
+            </li>
+            <li>
+                <h2>BIENVENIDO! &nbsp;</h2>
+                <asp:Label ID="Label6" runat="server" Text="Usuario"></asp:Label>
+            &nbsp;<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                <br />
+                <br />
+                Contraseña
+                <asp:TextBox ID="TextBox4" runat="server" TextMode="Password"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Button ID="Button1" runat="server" Text="INICIAR SESION" />
+            </li>
+            <li>
+                <h2>USUARIO NUEVO</h2>
+            </li>
+        </ul>
+    </div>
+</div><!-- #main -->
+
+
+<footer>
+</footer><!-- /footer -->
+
+
+
+</div><!-- /#wrapper -->
+
+
+</body>
 </asp:Content>
--- FASE 2/[IPC2]FASE2_201224240/[IPC2]FASE2_201224240/Default.aspx.designer.cs
+++ FASE 2/[IPC2]FASE2_201224240/[IPC2]FASE2_201224240/Default.aspx.designer.cs
@@ -11,5 +11,113 @@ namespace _IPC2_FASE2_201224240 {
     
     
     public partial class _Default {
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Control ListBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+        
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+        
+        /// <summary>
+        /// Control TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// Control TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
