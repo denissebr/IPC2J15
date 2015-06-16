5053e79eeaaa3d3205e6c766248fedb0de21f15d
denissebr <joziitha642@gmail.com>
2015-06-15 19:23:59 -0600

    actualizacion de campos

--- PRACTICA 1/ScriptBD.sql
+++ PRACTICA 1/ScriptBD.sql
@@ -3,7 +3,7 @@ CREATE DATABASE TareaPractica1
 USE TareaPractica1
 
 CREATE TABLE Cliente(
-Numero_cliente  INT IDENTITY (1,1) PRIMARY KEY,
+Carnet  int PRIMARY KEY,
 Nombre VARCHAR(MAX) NOT NULL,
 DPI BIGINT NOT NULL,
 Direccion VARCHAR(MAX) NOT NULL,
@@ -11,21 +11,21 @@ Telefono BIGINT NOT NULL
 )
 
 CREATE TABLE Libro(
-Id_Libro INT IDENTITY (1,1) PRIMARY KEY,
-Nombre_libro VARCHAR(MAX) NOT NULL,
-Num_Existencias INT NOT NULL,
-Num_Paginas INT NOT NULL,
-Autor_libro VARCHAR(MAX) NOT NULL,
-Tema_libro VARCHAR(MAX) NOT NULL,
-Existencia_en_libreria INT NOT NULL,
+IdLibro INT IDENTITY (1,1) PRIMARY KEY,
+Nombrelibro VARCHAR(MAX) NOT NULL,
+NumExistencias INT NOT NULL,
+NumPaginas INT NOT NULL,
+Autorlibro VARCHAR(MAX) NOT NULL,
+Temalibro VARCHAR(MAX) NOT NULL,
+Disponibles INT NOT NULL,
 Prestamos INT NOT NULL,
 Reserva Int NOT NULL
 )
 
 CREATE TABLE Prestamo(
-Id_Prestamo INT IDENTITY (1,1) PRIMARY KEY,
-IdCliente INT NOT NULL,
-Id_Libro INT NOT NULL,
-FOREIGN KEY (IdCliente) REFERENCES Cliente(Numero_cliente),
-FOREIGN KEY (Id_Libro) REFERENCES Libro(Id_Libro)
+IdPrestamo INT IDENTITY (1,1) PRIMARY KEY,
+Carnet INT NOT NULL,
+IdLibro INT NOT NULL,
+FOREIGN KEY (Carnet) REFERENCES Cliente(Carnet),
+FOREIGN KEY (IdLibro) REFERENCES Libro(IdLibro)
 )
